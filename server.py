7f1389c18d7365d5154a9e3dc5cb3c91a051eda2
Mark Music <me@markmusic.io>
2025-09-13 20:24:32 -0600

    feat: setup logging

--- server.py
+++ server.py
@@ -1,5 +1,16 @@
 import logging
+import click
 
 logger = logging.getLogger(__name__)
 
-logger.info("hello world")
+@click.command()
+@click.option("--port", default="8000", help="Port to listen on for HTTP")
+@click.option("--log-level", default="INFO", help="Logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)",)
+def main(port: int, log_level: str):
+    
+    # Configure logging
+    logging.basicConfig(
+        level=getattr(logging, log_level.upper()), # -> converts log_level to logging.{log_level} (e.g, INFO -> logging.INFO)
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+    
